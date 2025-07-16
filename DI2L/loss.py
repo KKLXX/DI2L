f61dcc7ddf59286e022d32323d386da16711d16c
KKLXX <85377411+KKLXX@users.noreply.github.com>
2025-07-16 11:21:29 +0800

    Add files via upload

---
+++ DI2L/loss.py
@@ -0,0 +1,285 @@
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd.function import Function
+from torch.autograd import Variable
+import pdb
+
+class OriTripletLoss(nn.Module):
+    """Triplet loss with hard positive/negative mining.
+    
+    Reference:
+    Hermans et al. In Defense of the Triplet Loss for Person Re-Identification. arXiv:1703.07737.
+    Code imported from https://github.com/Cysu/open-reid/blob/master/reid/loss/triplet.py.
+    
+    Args:
+    - margin (float): margin for triplet.
+    """
+
+
+    def __init__(self, batch_size, margin=0.3):
+        super(OriTripletLoss, self).__init__()
+        self.margin = margin
+        self.ranking_loss = nn.MarginRankingLoss(margin=margin)
+
+    def forward(self, inputs, targets):
+        """
+        Args:
+        - inputs: feature matrix with shape (batch_size, feat_dim)
+        - targets: ground truth labels with shape (num_classes)
+        """
+        n = inputs.size(0)
+        
+        # Compute pairwise distance, replace by the official when merged
+        dist = torch.pow(inputs, 2).sum(dim=1, keepdim=True).expand(n, n)
+        dist = dist + dist.t()
+        dist.addmm_(1, -2, inputs, inputs.t())
+        dist = dist.clamp(min=1e-12).sqrt()  # for numerical stability
+        
+        # For each anchor, find the hardest positive and negative
+        mask = targets.expand(n, n).eq(targets.expand(n, n).t())
+        dist_ap, dist_an = [], []
+        for i in range(n):
+            dist_ap.append(dist[i][mask[i]].max().unsqueeze(0))
+            dist_an.append(dist[i][mask[i] == 0].min().unsqueeze(0))
+        dist_ap = torch.cat(dist_ap)
+        dist_an = torch.cat(dist_an)
+        
+        # Compute ranking hinge loss
+        y = torch.ones_like(dist_an)
+        loss = self.ranking_loss(dist_an, dist_ap, y)
+        
+        # compute accuracy
+        correct = torch.ge(dist_an, dist_ap).sum().item()
+        return loss, correct    
+        
+# Adaptive weights
+def softmax_weights(dist, mask):
+    max_v = torch.max(dist * mask, dim=1, keepdim=True)[0]
+    diff = dist - max_v
+    Z = torch.sum(torch.exp(diff) * mask, dim=1, keepdim=True) + 1e-6 # avoid division by zero
+    W = torch.exp(diff) * mask / Z
+    return W
+
+def normalize(x, axis=-1):
+    """Normalizing to unit length along the specified dimension.
+    Args:
+      x: pytorch Variable
+    Returns:
+      x: pytorch Variable, same shape as input
+    """
+    x = 1. * x / (torch.norm(x, 2, axis, keepdim=True).expand_as(x) + 1e-12)
+    return x
+
+class TripletLoss_WRT(nn.Module):
+    """Weighted Regularized Triplet'."""
+
+    def __init__(self):
+        super(TripletLoss_WRT, self).__init__()
+        self.ranking_loss = nn.SoftMarginLoss()
+
+    def forward(self, inputs, targets, normalize_feature=False):
+        if normalize_feature:
+            inputs = normalize(inputs, axis=-1)
+        dist_mat = pdist_torch(inputs, inputs)
+
+        N = dist_mat.size(0)
+        # shape [N, N]
+        is_pos = targets.expand(N, N).eq(targets.expand(N, N).t()).float()
+        is_neg = targets.expand(N, N).ne(targets.expand(N, N).t()).float()
+
+        # `dist_ap` means distance(anchor, positive)
+        # both `dist_ap` and `relative_p_inds` with shape [N, 1]
+        dist_ap = dist_mat * is_pos
+        dist_an = dist_mat * is_neg
+
+        weights_ap = softmax_weights(dist_ap, is_pos)
+        weights_an = softmax_weights(-dist_an, is_neg)
+        furthest_positive = torch.sum(dist_ap * weights_ap, dim=1)
+        closest_negative  = torch.sum(dist_an * weights_an, dim=1)
+
+        y = furthest_positive.new().resize_as_(furthest_positive).fill_(1)
+        loss = self.ranking_loss(closest_negative - furthest_positive, y)
+
+        # compute accuracy
+        correct = torch.ge(closest_negative, furthest_positive).sum().item()
+        return loss, correct
+
+class TripletLoss_ADP(nn.Module):
+    """Weighted Regularized Triplet'."""
+
+    def __init__(self, alpha =1, gamma = 1, square = 0):
+        super(TripletLoss_ADP, self).__init__()
+        self.ranking_loss = nn.SoftMarginLoss()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.square = square
+
+    def forward(self, inputs, targets, normalize_feature=False):
+        if normalize_feature:
+            inputs = normalize(inputs, axis=-1)
+        dist_mat = pdist_torch(inputs, inputs)
+
+        N = dist_mat.size(0)
+        # shape [N, N]
+        is_pos = targets.expand(N, N).eq(targets.expand(N, N).t()).float()
+        is_neg = targets.expand(N, N).ne(targets.expand(N, N).t()).float()
+
+        # `dist_ap` means distance(anchor, positive)
+        # both `dist_ap` and `relative_p_inds` with shape [N, 1]
+        dist_ap = dist_mat * is_pos
+        dist_an = dist_mat * is_neg
+
+        weights_ap = softmax_weights(dist_ap*self.alpha, is_pos)
+        weights_an = softmax_weights(-dist_an*self.alpha, is_neg)
+        furthest_positive = torch.sum(dist_ap * weights_ap, dim=1)
+        closest_negative = torch.sum(dist_an * weights_an, dim=1)
+
+        
+        # ranking_loss = nn.SoftMarginLoss(reduction = 'none')
+        # loss1 = ranking_loss(closest_negative - furthest_positive, y)
+        
+        # squared difference
+        if self.square ==0:
+            y = furthest_positive.new().resize_as_(furthest_positive).fill_(1)
+            loss = self.ranking_loss(self.gamma*(closest_negative - furthest_positive), y)
+        else:
+            diff_pow = torch.pow(furthest_positive - closest_negative, 2) * self.gamma
+            diff_pow =torch.clamp_max(diff_pow, max=88)
+
+            # Compute ranking hinge loss
+            y1 = (furthest_positive > closest_negative).float()
+            y2 = y1 - 1
+            y = -(y1 + y2)
+            
+            loss = self.ranking_loss(diff_pow, y)
+        
+        # loss = self.ranking_loss(self.gamma*(closest_negative - furthest_positive), y)
+
+        # compute accuracy
+        correct = torch.ge(closest_negative, furthest_positive).sum().item()
+        return loss, correct
+
+      
+class KLDivLoss(nn.Module):
+    def __init__(self):
+        super(KLDivLoss, self).__init__()
+    def forward(self, pred, label):
+        # pred: 2D matrix (batch_size, num_classes)
+        # label: 1D vector indicating class number
+        T=3
+
+        predict = F.log_softmax(pred/T,dim=1)
+        target_data = F.softmax(label/T,dim=1)
+        target_data =target_data+10**(-7)
+        target = Variable(target_data.data.cuda(),requires_grad=False)
+        loss=T*T*((target*(target.log()-predict)).sum(1).sum()/target.size()[0])
+        return loss
+
+class focal_loss(nn.Module):
+    def __init__(self, alpha=0.25, gamma=2, num_classes = 3, size_average=True):
+        """
+        focal_loss损失函数, -α(1-yi)**γ *ce_loss(xi,yi)
+        步骤详细的实现了 focal_loss损失函数.
+        :param alpha:   阿尔法α,类别权重.      当α是列表时,为各类别权重,当α为常数时,类别权重为[α, 1-α, 1-α, ....],常用于 目标检测算法中抑制背景类 , retainnet中设置为0.25
+        :param gamma:   伽马γ,难易样本调节参数. retainnet中设置为2
+        :param num_classes:     类别数量
+        :param size_average:    损失计算方式,默认取均值
+        """
+
+        super(focal_loss,self).__init__()
+        self.size_average = size_average
+        if isinstance(alpha,list):
+            assert len(alpha)==num_classes   # α可以以list方式输入,size:[num_classes] 用于对不同类别精细地赋予权重
+            print("Focal_loss alpha = {}, 将对每一类权重进行精细化赋值".format(alpha))
+            self.alpha = torch.Tensor(alpha)
+        else:
+            assert alpha<1   #如果α为一个常数,则降低第一类的影响,在目标检测中为第一类
+            print(" --- Focal_loss alpha = {} ,将对背景类进行衰减,请在目标检测任务中使用 --- ".format(alpha))
+            self.alpha = torch.zeros(num_classes)
+            self.alpha[0] += alpha
+            self.alpha[1:] += (1-alpha) # α 最终为 [ α, 1-α, 1-α, 1-α, 1-α, ...] size:[num_classes]
+        self.gamma = gamma
+
+    def forward(self, preds, labels):
+        """
+        focal_loss损失计算
+        :param preds:   预测类别. size:[B,N,C] or [B,C]    分别对应与检测与分类任务, B 批次, N检测框数, C类别数
+        :param labels:  实际类别. size:[B,N] or [B]
+        :return:
+        """
+        # assert preds.dim()==2 and labels.dim()==1
+        preds = preds.view(-1,preds.size(-1))
+        self.alpha = self.alpha.to(preds.device)
+        preds_softmax = F.softmax(preds, dim=1) # 这里并没有直接使用log_softmax, 因为后面会用到softmax的结果(当然你也可以使用log_softmax,然后进行exp操作)
+        preds_logsoft = torch.log(preds_softmax)
+        preds_softmax = preds_softmax.gather(1,labels.view(-1,1))   # 这部分实现nll_loss ( crossempty = log_softmax + nll )
+        preds_logsoft = preds_logsoft.gather(1,labels.view(-1,1))
+        self.alpha = self.alpha.gather(0,labels.view(-1))
+        loss = -torch.mul(torch.pow((1-preds_softmax), self.gamma), preds_logsoft)  # torch.pow((1-preds_softmax), self.gamma) 为focal loss中 (1-pt)**γ
+        loss = torch.mul(self.alpha, loss.t())
+        if self.size_average:
+            loss = loss.mean()
+        else:
+            loss = loss.sum()
+        return loss
+
+class FocalLoss(nn.Module):
+    """
+    参考 https://github.com/lonePatient/TorchBlocks
+    """
+
+    def __init__(self, gamma=1.6, alpha=1, epsilon=1.e-9, device=None):
+        super(FocalLoss, self).__init__()
+        self.gamma = gamma
+        if isinstance(alpha, list):
+            self.alpha = torch.Tensor(alpha, device=device)
+        else:
+            self.alpha = alpha
+        self.epsilon = epsilon
+
+    def forward(self, input, target):
+        """
+        Args:
+            input: model's output, shape of [batch_size, num_cls]
+            target: ground truth labels, shape of [batch_size]
+        Returns:
+            shape of [batch_size]
+        """
+        num_labels = input.size(-1)
+        idx = target.view(-1, 1).long()
+        one_hot_key = torch.zeros(idx.size(0), num_labels, dtype=torch.float32, device=idx.device)
+        one_hot_key = one_hot_key.scatter_(1, idx, 1)
+        logits = torch.softmax(input, dim=-1)
+        loss = -self.alpha * one_hot_key * torch.pow((1 - logits), self.gamma) * (logits + self.epsilon).log()
+        loss = loss.sum(1)
+        return loss.mean()
+
+       
+def pdist_torch(emb1, emb2):
+    '''
+    compute the eucilidean distance matrix between embeddings1 and embeddings2
+    using gpu
+    '''
+    m, n = emb1.shape[0], emb2.shape[0]
+    emb1_pow = torch.pow(emb1, 2).sum(dim = 1, keepdim = True).expand(m, n)
+    emb2_pow = torch.pow(emb2, 2).sum(dim = 1, keepdim = True).expand(n, m).t()
+    dist_mtx = emb1_pow + emb2_pow
+    dist_mtx = dist_mtx.addmm_(1, -2, emb1, emb2.t())
+    # dist_mtx = dist_mtx.clamp(min = 1e-12)
+    dist_mtx = dist_mtx.clamp(min = 1e-12).sqrt()
+    return dist_mtx    
+
+
+def pdist_np(emb1, emb2):
+    '''
+    compute the eucilidean distance matrix between embeddings1 and embeddings2
+    using cpu
+    '''
+    m, n = emb1.shape[0], emb2.shape[0]
+    emb1_pow = np.square(emb1).sum(axis = 1)[..., np.newaxis]
+    emb2_pow = np.square(emb2).sum(axis = 1)[np.newaxis, ...]
+    dist_mtx = -2 * np.matmul(emb1, emb2.T) + emb1_pow + emb2_pow
+    # dist_mtx = np.sqrt(dist_mtx.clip(min = 1e-12))
+    return dist_mtx
